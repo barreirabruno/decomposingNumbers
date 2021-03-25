df69c6c61d4b70cd24ee5565b4263abbf12f47c1
barreirabruno <brunobarreirasilva@gmail.com>
2021-03-25 10:42:43 -0300

    refactor: remove sysout and rename two variables

--- DecomposingNumbers/src/DecomposeNumber.java
+++ DecomposingNumbers/src/DecomposeNumber.java
@@ -5,39 +5,34 @@ import java.util.Random;
 public class DecomposeNumber {
 	public static List<Integer> calculation(int maxDigit) {
 		List<Integer> storeNumbers = new ArrayList<Integer>();
-		List<Integer> numbersSumTo21 = new ArrayList<Integer>();
-		System.out.println("[INPUT INTEGER]: " + maxDigit);
+		List<Integer> numbersSumToTwnetyOne = new ArrayList<Integer>();
+		List<String> FourDigitNumbers = new ArrayList<String>();
 		for(int index = 0; index <= maxDigit; index++) {
-			System.out.println("[GENERATING NUMBER " + index + "] " + index + " [ADDING TO A LIST]");
 			storeNumbers.add(index);
 		}
-		List<String> formedNumbers = new ArrayList<String>();
-		
 		for(int index = 0; index <= 9999; index++) {
 			String result = "";
 			for(int i = 0; i <= 3; i++) {
 				Integer randomNumFromList = getRandomElement(storeNumbers);
 				result += randomNumFromList.toString();
 			}
-			formedNumbers.add(result);
+			FourDigitNumbers.add(result);
 		}
 		
-		for(int index = 0; index <= formedNumbers.size()-1; index++) {
+		for(int index = 0; index <= FourDigitNumbers.size()-1; index++) {
 			int count = 0;
 			int sum = 0;
-			while(count <= formedNumbers.get(index).length()-1) {
-				Integer number = Character.getNumericValue(formedNumbers.get(index).charAt(count));
+			while(count <= FourDigitNumbers.get(index).length()-1) {
+				Integer number = Character.getNumericValue(FourDigitNumbers.get(index).charAt(count));
 				sum += number;
 				if(sum == 21) {
-					numbersSumTo21.add(Integer.parseInt(formedNumbers.get(index)));
+					numbersSumToTwnetyOne.add(Integer.parseInt(FourDigitNumbers.get(index)));
 				}
 				count++;
 			}
 		}
 		
-		System.out.println(numbersSumTo21);
-		
-		return numbersSumTo21;
+		return numbersSumToTwnetyOne;
  	}
 	
 	private static int getRandomElement(List<Integer> list) {
